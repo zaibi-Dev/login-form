2a10df397bb96d1f24cbeccd8cc87b6962ad3bd0
shahzaib aziz <ranazabi026@gmail.com>
2026-01-19 15:15:07 +0500

    Add files via upload
    Add login form project

---
+++ script.js
@@ -0,0 +1,33 @@
+function login(){
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+
+    let emailError =document.getElementById("emailError");
+    let passError=document.getElementById("passError");
+    let successMsj=document.getElementById("successMsj")
+     emailError.innerText = "";
+     passError.innerText = "";
+     successMsg.innerText = "Not success";
+
+     if (email === "") {
+    emailError.innerText = "Email is required";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    emailError.innerText = "Invalid email";
+    return;
+  }
+     if(password===""){
+        passError.innerText="password is required";
+        return;
+     }
+    if (password.length <6 )
+
+        {
+    passError.innerText="minimum 6 characters";
+    return;
+        }
+    successMsg.innerText="login successfull";
+
+}
